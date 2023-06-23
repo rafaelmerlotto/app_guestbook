c8e68da61243f20ceaed97c1563ea7edb5ebd61d
Rafael Merlotto <rafaelmerlottogmail.com>
2023-06-23 22:52:26 +0200

    modifiche

--- gioco/index.js
+++ gioco/index.js
@@ -6,27 +6,25 @@ const btn = document.querySelector('.btn');
 const messaggio = document.querySelector('.messaggio')
 const user1 = document.querySelector(".user1");
 const user2 = document.querySelector(".user2");
+const listato = document.querySelector(".listato");
+const counter = document.querySelector('.counter')
+let punteggio1 = 0;
+let punteggio2 = 0;
+const body = document;
+const party = new JSConfetti({ body });
 
-btn.addEventListener('click', mossePossibile)
+btn.addEventListener('click', mossePossibile);
 
 function mossePossibile() {
-
-
     const armi = ['🪨', '📄', '✂️'];
     const giocata1 = armi[Math.floor(Math.random() * armi.length)];
     const giocata2 = armi[Math.floor(Math.random() * armi.length)];
 
-
-    determinaVittoria(giocata1, giocata2);
-
     giocatore1.innerText = giocata1;
     giocatore2.innerText = giocata2;
 
-
-
+    determinaVittoria(giocata1, giocata2);
 }
-
-
 
 function determinaVittoria(mossaGiocatore1, mossaGiocatore2) {
     if (
@@ -34,36 +32,24 @@ function determinaVittoria(mossaGiocatore1, mossaGiocatore2) {
         (mossaGiocatore1 === "✂️" && mossaGiocatore2 === "📄") ||
         (mossaGiocatore1 === "🪨" && mossaGiocatore2 === "✂️")
     ) {
-        const win1 = [messaggio.innerText = `${user1.value} ha vinto 🎉`];
-    
-            
-        console.log(win1)
-
-
+        punteggio1++;
+        if (punteggio1 === 3) {
+            party.addConfetti();
+            messaggio.innerText = `${user1.value} ha vinto 🎉`
+        }
     } else if (
         (mossaGiocatore1 === "🪨" && mossaGiocatore2 === "📄") ||
         (mossaGiocatore1 === "📄" && mossaGiocatore2 === "✂️") ||
         (mossaGiocatore1 === "✂️" && mossaGiocatore2 === "🪨")
     ) {
-        const win2  = `${user2.value} ha vinto 🎉`;
-       
-            const salva2 = [];
-            salva2.push(win2);
-            console.log(salva2)
-    
-
-
-
-
+        punteggio2++;
+        if (punteggio2 === 3) {
+            party.addConfetti();
+            messaggio.innerText = `${user2.value} ha vinto 🎉`
+        }
     } else {
         messaggio.innerText = "Pareggio";
-
     }
-
+    counter.innerText = `${user1.value} ${punteggio1} X ${punteggio2} ${user2.value} `
 }
-
-
-
-
-
 
